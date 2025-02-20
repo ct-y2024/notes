c401d91d62712eca5682b19a352275d5dc480487
SlavaChepelin <slavachepelin@mail.ru>
2025-02-20 07:39:11 +0300

    published some solutions for problem

--- docs/scripts/sidebar.js
+++ docs/scripts/sidebar.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
     const toggleButton = document.getElementById("toggle-button");
     const sidebar = document.getElementById("sidebar");
+    const body = document.body; // Получаем элемент body
 
     toggleButton.addEventListener("click", function() {
         sidebar.classList.toggle("hidden");
+        body.classList.toggle("sidebar-hidden"); // Добавляем/удаляем класс для body
     });
 });
-
